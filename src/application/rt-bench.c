f5688a44b902520c7a283cdbcd64365e76f9b3ce
user <user@doppler.control.lth.se>
2015-03-26 14:29:56 +0100

    Changed repository

--- src/application/rt-bench.c
+++ src/application/rt-bench.c
@@ -29,13 +29,11 @@ void memory (int ind, ...) {
   int loops;
   struct timespec *t_spec;
   va_list argp;
-  int change;
   va_start(argp, ind);
   t_spec = va_arg(argp, struct timespec*);
   loops = timespec_to_usec(t_spec); // this contains the amount of mathematical operations to be done
   memory_used = va_arg(argp, int); // this contains the amount of memory to be allocated
-  change = va_arg(argp, int);
-  printf("this seems like a stupid thing to do\n")
+
   va_end(argp);
   // TODO: implement the memory phase
 }
