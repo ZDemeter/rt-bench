3e7ee87b7f93a0ee3dba1b5043787529ac6c1b17
Zsolt Demeter <zolledemeter@gmail.com>
2015-04-21 12:23:56 +0200

    Searching for errors

--- launch.sh
+++ launch.sh
@@ -128,7 +128,7 @@ echo "% ----------------------------------------" >> $GENERATED_OCTAVE_SCRIPT
 echo "addpath('$ANALYSIS_DIR');" >> $GENERATED_OCTAVE_SCRIPT
 echo "addpath('${ANALYSIS_DIR}jsonlab/');" >> $GENERATED_OCTAVE_SCRIPT
 echo "process(experiment_name);" >> $GENERATED_OCTAVE_SCRIPT
-echo "disp ("can I print here?");" >> $GENERATED_OCTAVE_SCRIPT
+echo "disp("allow me to print plz")" >> $GENERATED_OCTAVE_SCRIPT
 echo "analysis;" >> $GENERATED_OCTAVE_SCRIPT
 
 printf "[ERROR PASS] ?--no-window-system?\n"
